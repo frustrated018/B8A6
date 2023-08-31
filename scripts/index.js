0b144250884d9f15e8055ebb10cca28d9f5b619b
frustrated018 <niloyd018@gmail.com>
2023-09-01 02:02:36 +0600

    Made the different category section.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log('Script is working');
+const handleCatagory = async () => {
+  const tabContainer = document.getElementById("tab-container");
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const apiData = data.data;
+  apiData.forEach((tabName) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <a onclick = "handleLoadNews(${tabName.category_id})" class="tab">${tabName.category}</a>
+        `;
+    tabContainer.appendChild(div);
+  });
+
+  // console.log(data.data);
+};
+
+const handleLoadNews = (id) => {
+    console.log(id);
+};
+
+handleCatagory();
