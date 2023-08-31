7550491e1cdc3382627a2ed7b4c82f7d8383bad9
frustrated018 <niloyd018@gmail.com>
2023-09-01 03:21:21 +0600

    Made a basic card container

--- scripts/index.js
+++ scripts/index.js
@@ -17,9 +17,39 @@ const handleCatagory = async () => {
 };
 // fetching different catagory contents
 const handleLoadNews = async (id) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
-    const data = await res.json();
-    console.log(data);
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const data = await res.json();
+
+  data.data.forEach((video) => {
+    const cardsContainer = document.getElementById("cards-container");
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+    <div class="card card-compact bg-base-100 shadow-xl">
+                <figure><img src="${video?.thumbnail}"></figure>
+                <div class="card-body">
+                    <!-- Profile photo and title -->
+                    <div class="flex flex-row gap-2 items-center">
+                        <div class="avatar">
+                            <div class="w-10 rounded-full">
+                                <img src="${video?.authors[0]?.profile_picture}">
+                            </div>
+                        </div>
+                        <h3 class="text-base text-neutral-900 font-bold leading-relaxed">${video?.title}</h3>
+                    </div>
+                    <!-- Creator name and blue check -->
+                    <div class="flex flex-row gap-2">
+                        <div class="text-neutral-900 text-opacity-70 text-sm font-normal">${video?.authors[0]?.profile_name}</div>
+                            <span>${video?.authors[0]?.verified}</span>
+                        </div>
+                    <!-- View count -->
+                    <div class="text-neutral-900 text-opacity-70 text-sm font-normal">91K views</div>
+                </div>
+            </div>
+        `;
+    cardsContainer.appendChild(newDiv);
+  });
 };
 
 handleCatagory();
